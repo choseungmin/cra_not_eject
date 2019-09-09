61b17cc4ecb08d69882393a0e8e3617a273ef0ac
seungmin <mirame@nextree.co.kr>
2019-09-09 15:00:22 +0900

    store 및 inject 사용

--- src/Counter.js
+++ src/Counter.js
@@ -1,41 +1,27 @@
 import React, { Component } from 'react';
-import { observable, action } from 'mobx';
-import { observer } from 'mobx-react';
+import { observer, inject } from 'mobx-react';
 
-// **** 최하단에 잇던 observer 가 이렇게 위로 올라옵니다.
+/*
+// **** 함수형태로 파라미터를 전달해주면 특정 값만 받아올 수 있음.
+@inject(stores => ({
+  number: stores.counter.number,
+  increase: stores.counter.increase,
+  decrease: stores.counter.decrease,
+}))
+*/
+@inject('counter')
 @observer
 class Counter extends Component {
-  @observable number = 0;
-
-  @action
-  increase = () => {
-    this.number++;
-  }
-
-  @action
-  decrease = () => {
-    this.number--;
-  }
-
   render() {
+    const { counter } = this.props;
     return (
       <div>
-        <h1>{this.number}</h1>
-        <button onClick={this.increase}>+1</button>
-        <button onClick={this.decrease}>-1</button>
+        <h1>{counter.number}</h1>
+        <button onClick={counter.increase}>+1</button>
+        <button onClick={counter.decrease}>-1</button>
       </div>
     );
   }
 }
 
-
-// **** decorate 는 더 이상 필요 없어집니다.
-// decorate(Counter, {
-//   number: observable,
-//   increase: action,
-//   decrease: action
-// })
-
-// export default observer(Counter);
-// **** observer 는 코드의 상단으로 올라갑니다.
 export default Counter;
